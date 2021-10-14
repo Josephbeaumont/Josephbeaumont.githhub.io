534dbdf8f73375f30081d5f6bab6c45cd812a67c
Josephbeaumont <gghi1627317b@gmail.com>
2021-10-14 13:18:05 +0000

    animation

--- NEEDS TO BE DEBUGED html canvas/script.js
+++ NEEDS TO BE DEBUGED html canvas/script.js
@@ -1,36 +1,36 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
-canvas.Height = window.innerHeight;
+canvas.eight = window.innerHeight;
 let particlesArray = [];
 
 class Particle {
     constructor(x, y){
         this.x = x;
         this.y = y;
-        this.size = 10;
-        this.weight = 2;
-        this.directionX - 1;
+        this.size = 4.5;
+        this.weight = 1;
+        this.directionX = -1;
     }
     update(){
         if (this.y > canvas.height) {
             this.y = 0 - this.size;
             this.weight = 2;
-            this.x = Math.random() * canvas.width; 
+            this.x = Math.random() * canvas.width;
         }
-        this.weight += 0.1;
+        this.weight += 0.5;
         this.y += this.weight;
         this.x += this.directionX;
     }
     draw(){
         ctx.fillStyle = 'red';
         ctx.beginPath();
-        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.arc(this.x, this.y, this.size, 1, Math.PI * 1);
         ctx.closePath();
         ctx.fill()
     }
 }
-    const particle1 = new Particle(400, 100);
+    const particle1 = new Particle(20, 90);
 
     function animate(){
         ctx.fillStyle = 'rgba(255, 255, 255, 0.01)';
