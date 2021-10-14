0a83fe9968e93fd08694b7f958f5b4631eb528a6
Josephbeaumont <gghi1627317b@gmail.com>
2021-10-14 14:13:47 +0000

    change variables

--- NEEDS TO BE DEBUGED html canvas/script.js
+++ NEEDS TO BE DEBUGED html canvas/script.js
@@ -8,29 +8,29 @@ class Particle {
     constructor(x, y){
         this.x = x;
         this.y = y;
-        this.size = 4.5;
-        this.weight = 1;
-        this.directionX = -1;
+        this.size = 9;
+        this.weight = 2;
+        this.directionX = -2;
     }
     update(){
         if (this.y > canvas.height) {
             this.y = 0 - this.size;
-            this.weight = 2;
+            this.weight = 0;
             this.x = Math.random() * canvas.width;
         }
-        this.weight += 0.5;
+        this.weight += 0.01;
         this.y += this.weight;
         this.x += this.directionX;
     }
     draw(){
         ctx.fillStyle = 'red';
         ctx.beginPath();
-        ctx.arc(this.x, this.y, this.size, 1, Math.PI * 1);
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 1);
         ctx.closePath();
         ctx.fill()
     }
 }
-    const particle1 = new Particle(20, 90);
+    const particle1 = new Particle(400, 100);
 
     function animate(){
         ctx.fillStyle = 'rgba(255, 255, 255, 0.01)';
