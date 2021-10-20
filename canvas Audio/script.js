811a96f29e8fd0d5ed1d5c2c1dcde590ab39ba9f
Josephbeaumont <gghi1627317b@gmail.com>
2021-10-20 17:42:37 +0000

    audio visual

--- canvas Audio/script.js
+++ canvas Audio/script.js
@@ -1,13 +1,20 @@
-const button1 = document.getElementById('button1');
-let Audio1 = new Audio();
-Audio1.src = 'music.mp3';
+const audioCtx = new AudioContext();
 
- button1.addEventListener('click', function(){
-     Audio1.play();
-     Audio1.addEventListener('playing', function(){
-         console.log();
- });
- Audio1.addEventListener('ended', function(){
-     
- })
+const container = document.getElementById('container1');
+const canvas = document.getElementById('canvas1');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const ctx = canvas.getContext('2d');
+let audioSource;
+let analyser;
+
+container.addEventListener('click', function(){
+    Audio1.play();
+    audioSource = audioContext.createMediaElementSource(audio1);
+    analyser = audioContext.createAnalyser();
+    audioSource.connect(analyser);
+    analyser.connect(audioContext.destination);
+    analyser.fftSize = 64;
+    const bufferlenght = analyser.frequencyBinCount;
 });
+//time stamp 25:56
