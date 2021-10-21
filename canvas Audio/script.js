5152efa89c683c40e92c2efa58267cdc04188b2b
Josephbeaumont <gghi1627317b@gmail.com>
2021-10-21 13:10:34 +0000

    1

--- canvas Audio/script.js
+++ canvas Audio/script.js
@@ -1,5 +1,5 @@
-const audioCtx = new AudioContext();
 
+const audioCtx = new AudioContext();
 const container = document.getElementById('container1');
 const canvas = document.getElementById('canvas1');
 canvas.width = window.innerWidth;
@@ -9,12 +9,35 @@ let audioSource;
 let analyser;
 
 container.addEventListener('click', function(){
+    const Audio1 = document.getElementById('Audio1');
+    const Audio2 = document.getElementById('Audio2');
+    Audio1.src ='music.mp3';
+    Audio2.src ='Cinematic.mp3';
     Audio1.play();
-    audioSource = audioContext.createMediaElementSource(audio1);
+    audioSource = audioContext.createMediaElementSource(Audio1);
     analyser = audioContext.createAnalyser();
     audioSource.connect(analyser);
     analyser.connect(audioContext.destination);
     analyser.fftSize = 64;
-    const bufferlenght = analyser.frequencyBinCount;
+    const bufferLenght = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLenght);
+
+    const barWidth = canvas.width/bufferLenght;
+    let barHeight;
+    let x =0;
+
+    function animate(){
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        analyser.getByteFrequencyData(dataArray);
+        for (let i = 0; i< bufferLengh; i++){
+            barHeight = dataArray[i];
+            ctx.fillStyle = 'white';
+            ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
+            x += barWidth;
+
+
+        }
+        requestAnimationFrame(animate);
+    }
+    animate();
 });
-//time stamp 25:56
