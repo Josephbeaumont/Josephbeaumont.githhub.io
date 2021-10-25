1510f901ae16cee3bcb19a2362a908cc0b1241c2
Josephbeaumont <gghi1627317b@gmail.com>
2021-10-25 13:09:18 +0000

    i really dont know what im doing or trying to do

---
+++ new folder/script.js
@@ -0,0 +1,7 @@
+<script type="text/javascript"></script>
+function draw() {
+    var canvas = document.geetElementById('tutorial');
+    if (canvas.getContext) {
+        var ctx = canvas.getContext('2d');
+    }
+}
