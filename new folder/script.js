cd476067f16bebc12f7bfe5d985a5961cba67d83
Josephbeaumont <gghi1627317b@gmail.com>
2021-10-25 17:52:44 +0000

    2 ball collsion

--- new folder/script.js
+++
@@ -1,7 +0,0 @@
-<script type="text/javascript"></script>
-function draw() {
-    var canvas = document.geetElementById('tutorial');
-    if (canvas.getContext) {
-        var ctx = canvas.getContext('2d');
-    }
-}
