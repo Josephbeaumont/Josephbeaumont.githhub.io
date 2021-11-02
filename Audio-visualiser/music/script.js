a70df576cf96ce7d110ebb393a871a98e6d2ee9a
Josephbeaumont <gghi1627317b@gmail.com>
2021-11-02 13:16:15 +0000

    mouse particles

--- Audio-visualiser/music/script.js
+++ Audio-visualiser/music/script.js
@@ -17,11 +17,11 @@ container.addEventListener('click', function () {
     analyser = audioContext.createAnalyser();
     audioSource.connect(analyser);
     analyser.connect(audioContext.destination);
-    analyser.fftSize = 16384; 
+    analyser.fftSize = 8192; 
     const bufferLength = analyser.frequencyBinCount;
     const dataArray = new Uint8Array(bufferLength);
 // number of bars 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768;
-    const barWidth = 0.200;
+    const barWidth = 0.800;
     let barHeight;
     let x = 0;
 
@@ -48,3 +48,59 @@ function drawVisualiser(bufferLength, x, barWidth, barHeight, dataArray){
         ctx.restore();
     }  
 }
+
+// Mouse particle Effects
+
+    // canvas setup
+    const canvas = document.getElementById('canvas2');
+    const ctx = canvas.getContext('2d');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    const particleArray = [];
+
+    // get mouse
+    const mouse = {
+        x: null,
+        y: null
+    }
+    window.addEventListener('mousemove', function(event){
+        mouse.x = event.x;
+        mouse.y = event.y;
+        //console.log(mouse.x, mouse.y);
+    });
+    setInterval(function(){
+        mouse.x = undefined;
+        mouse.y = undefined;
+    }, 200);
+
+    // Create Particles
+    class Particle {
+        constructor(x, y, size, color, weight){
+            this.x =x;
+            this.y =y;
+            this.size = size;
+            this.color = color;
+            this.weight = weight;
+        }
+        draw(){
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
+            ctx.fillStyle = this.color;
+            ctx.fill();
+       }
+        update(){
+            this.size -= 0.05;
+            if (this.size < 0){
+                this.x = (mouse.x + ((Math.random() * 20) -10));
+                this.y = (mouse.y + ((Math.random() * 20) -10));
+                this.size = (Math.random() * 10) + 2;
+                this.weight = (Math.random() * 2) - 0.5;
+            }
+            this.y += this.weight;
+            this.weight += 0.2;
+
+            if (this.y > canvas.height - this.size){
+                this.weight *= -1;
+            };
+        }
+   }
